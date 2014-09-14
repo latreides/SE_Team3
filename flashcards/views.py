8537d879cc09af6bb054143921eacde643ebae63
Leto Atreides <thedevilsjester@gmail.com>
2014-09-14 01:20:52 -0500

    Added custom context data to the landing page template.

--- flashcards/views.py
+++ flashcards/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 
 class LandingPage(TemplateView):
     template_name = 'landing_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LandingPage, self).get_context_data(**kwargs)
+        context['example_value'] = "Example"
+        return context
