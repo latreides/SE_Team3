84fb3a08cfe918633dc91caa60266152bedd0184
Scott21247 <chiefs21247@yahoo.com>
2014-09-16 22:50:06 -0500

    Added comments to the script telling what each field in all tables represents

--- flashcards/models.py
+++ flashcards/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 '''
 User is a database Table that will store information specific to a user
+    User_ID- Primary Key assigned by database when user created
+    Email- Email address of user
+    Password- User chosen password
+    Login_ID- User chosen Login ID
 '''
 class User(models.Model):
     User_ID = models.AutoField(primary_key=True)
@@ -18,6 +22,11 @@ class User(models.Model):
 
 '''
 Deck is a database table that stores information specific to a single deck
+    Deck_ID- (Primary Key) Integer value assigned when deck is created
+    User_ID- (Foreign Key) ID of the user who created the deck
+    Name- User chosen name of the deck of flashcards
+    Create_Date- Date which the deck was created
+    Accessed_Date- Last time the deck was used 
 '''
 class Deck(models.Model):
     Deck_ID = models.AutoField(primary_key=True)
@@ -36,6 +45,8 @@ class Deck(models.Model):
 '''
 Image is a database table that contains an ID for an image used for referencing in the card
 table, and then the path to where the image is stored on the server
+    Image_ID- (Primary Key) Integer assigned to card when it is stored
+    Image_Path- The path to the image file on the server
 '''
 class Image(models.Model):
     Image_ID = models.AutoField(primary_key=True)
@@ -50,6 +61,14 @@ class Image(models.Model):
 
 '''
 Card is a database table that stores information specific to a single flash card
+    Card_ID- (Primary Key) Integer assigned value for a card when it is created
+    Deck_ID- (Foreign Key) ID referencing the deck that the card is part of
+    Front/Back_Text- Text to display on the the corresponding side of the card
+    Front/Back_Img_ID- (Foreign Key) ID referencing the ID of the image to display
+    Difficulty- User rated difficulty of the current card
+    Last_Attempted- The most recent time the card was attempted by the user
+    Two_Sided- A boolean value hloding whether the card can be displayed with either
+    side forward 
 '''
 class Card(models.Model):
     Card_ID = models.AutoField(primary_key=True)
