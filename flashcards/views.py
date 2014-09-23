4a558a73c1baea1e1911a845b60080ac254a42cb
nnelson0 <flingsbottlecap@gmail.com>
2014-09-23 16:48:53 -0500

    My Decks & Manage Decks now read dicts of deck info
    My Decks and Manage Decks now use a dictionary filled with deck data,
    which is then read to populate the deck pane and button links. Code in
    views.py has the example dictionaries. Tiny CSS update for handling long
    Deck names.

--- flashcards/views.py
+++ flashcards/views.py
@@ -25,12 +25,42 @@ class LandingPage(LoginRedirect):
 
 class ManageDecksPage(LoginRedirect):
     template_name = 'manage_decks_page.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super(ManageDecksPage, self).get_context_data(**kwargs)
+        context['user_decks'] = [\
+			{"name":"How to Use MemorizeMe"}, \
+			{"name":"My Deck"}, \
+			{"name":"How To Swahili with Dr. Shade"}, \
+			{"name":"What is Love? (Baby, Don't Hurt Me)"}, \
+			{"name":"Identifying Wood"}, \
+			{"name":"Meine Flashkarte"}, \
+			{"name":"Mitt Flashcard"}, \
+			{"name":"Mi Tarjeta de Memoria Flash"} \
+			]
+		
+        return context
 
 class ScoresPage(LoginRedirect):
     template_name = 'scores_page.html'
 
 class ViewDeckPage(LoginRedirect):
     template_name = 'view_deck_page.html'
+	
+    def get_context_data(self, **kwargs):
+        context = super(ViewDeckPage, self).get_context_data(**kwargs)
+        context['user_decks'] = [\
+			{"name":"How to Use MemorizeMe", "playref":"#", "scoreref":"scores", "manref":"manage"}, \
+			{"name":"My Deck", "playref":"#", "scoreref":"scores", "manref":"manage"}, \
+			{"name":"How To Swahili with Dr. Shade", "playref":"#", "scoreref":"scores", "manref":"manage"}, \
+			{"name":"What is Love? (Baby, Don't Hurt Me)", "playref":"#", "scoreref":"scores", "manref":"manage"}, \
+			{"name":"Identifying Wood", "playref":"#", "scoreref":"scores", "manref":"manage"}, \
+			{"name":"Meine Flashkarte", "playref":"#", "scoreref":"scores", "manref":"manage"}, \
+			{"name":"Mitt Flashcard", "playref":"#", "scoreref":"scores", "manref":"manage"}, \
+			{"name":"Mi Tarjeta de Memoria Flash", "playref":"#", "scoreref":"scores", "manref":"manage"} \
+			]
+		
+        return context
 
 class AccountPage(LoginRedirect):
     template_name = 'account_page.html'
