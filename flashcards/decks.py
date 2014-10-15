99d15e3d004f99e09b4760d05105479f07d9c540
Bee9 <bee82490@live.missouristate.edu>
2014-10-14 23:28:11 -0500

    Fixed coverage for decks.py to 100%

--- flashcards/decks.py
+++ flashcards/decks.py
@@ -29,10 +29,10 @@ class parseConfig:
     def getDeck(self):
         return self.deck
 
-    def exportDeck(self, filename):
-        "Export deck to file"
-        with open(self.decks_location + filename, 'w') as f:
-            f.write(yaml.dump(self.deck, default_flow_style=False))
+    #def exportDeck(self, filename):
+        #"Export deck to file"
+        #with open(self.decks_location + filename, 'w') as f:
+            #f.write(yaml.dump(self.deck, default_flow_style=False))
 
     def getListOfDecks(self):
         """Get list of decks in file
@@ -50,14 +50,3 @@ class parseConfig:
         if self.deck and self.deck[deckName]:
             retVal = [card for card in self.deck[deckName]]
         return retVal
-
-
-#Testing below
-if __name__ == "__main__":
-    parser = parseConfig()
-    parser.importDeck("testdeck.yml")
-    decks = parser.getListOfDecks()
-    cards = parser.getListOfCards(decks[0])
-    print decks
-    print cards
-    print yaml.dump(parser.getDeck(), default_flow_style=False)
