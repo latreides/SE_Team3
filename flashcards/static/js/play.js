c0f83c84e3273794dc7bf3954195152410323dd6
nnelson0 <flingsbottlecap@gmail.com>
2014-10-22 18:34:03 -0500

    Applied suggestions/features to Play page
    JS in place for hotkeys, changed name of difficulty ranking buttons.
    Space flips the card. Once a card's been flipped, the rank buttons
    remain available until a new card is received (or page refreshed).

--- flashcards/static/js/play.js
+++ flashcards/static/js/play.js
@@ -1,15 +1,36 @@
+var cardHasBeenFlipped = false;
 var buttonsDisabled = false;
+var showingFront    = true;
+
+var orderOptions = ["frontFirst", "backFirst", "random"];
+var order = orderOptions[0];
 
 window.onload = function() {
     toggleButtons();
     $("#uiCard").click(flip);
 };
 
+document.addEventListener( "keyup", function(event) {
+        if( event.keyCode == 37 || event.keyCode == 39 || event.keyCode == 32 ||
+            event.keyCode == 49 || event.keyCode == 50 || event.keyCode == 51 ||
+            event.keyCode == 52 || event.keyCode == 53 ) {
+            // left, right, space,
+            // 1, 2, 3,
+            // 4, 5 (number row)
+            if (event.keyCode == 32) {
+                flip();
+            }
+        }
+    }
+);
+
 function loadXMLDoc() {
     var xmlhttp;
-    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
         xmlhttp=new XMLHttpRequest();
-    } else {// code for IE6, IE5
+    } else {
+        // code for IE6, IE5
         xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
     }
     xmlhttp.onreadystatechange=function() {
@@ -49,9 +70,19 @@ function toggleButtons() {
 }
 
 function flip() {
-    toggleButtons();
+    if (!cardHasBeenFlipped)
+        toggleButtons();
+    cardHasBeenFlipped = true;
+    showingFront = !showingFront;
+    $("#uiCardFront").toggleClass("hide")
+    $("#uiCardBack").toggleClass("hide")
+}
+
+function rate(rating) {
+    
 }
 
 function exitStageLeft() {
     // shove the current card out + left.
+    cardHasBeenFlipped = false;
 }
