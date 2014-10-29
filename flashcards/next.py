3de96d2b2bcbf7c4a70da2934a032c2ee3576de2
Bee9 <bee82490@live.missouristate.edu>
2014-10-29 10:07:49 -0500

    Fixed getNextCard function

--- flashcards/next.py
+++ flashcards/next.py
@@ -1,12 +1,20 @@
 from db_interactions import *
 from models import *
 from django.contrib.auth.models import User
-
+from random import randint
 
 def getNextCard():
     "Function to call next card."
     
-    deck = Deck.objects(pk = 1) #will need to modify, only for playing purposes
+    numOfDecks = Deck.objects.count()
+    deckID = randint(1, numOfDecks)
+    deck = Deck.objects.get(pk = deckID) #will need to modify, only for playing purposes
     deckOfCards = GetCardsForDeck(deck)
-    card = Card.objects.random(deckOfCards)
+    numOfCards = 0
+    
+    for card in deckOfCards:
+        numOfCards = numOfCards + 1
+        
+    cardID = randint(1, numOfCards)
+    card =  Card.objects.get(pk = cardID)
     return card 
